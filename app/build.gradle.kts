b5ff0b8ee8eb546d818c4e9e3a58ff56ac57963f
atfotiad <thfotiad@gmail.com>
2023-10-28 07:52:09 +0300

    Add Navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation ("androidx.navigation:navigation-compose:2.7.4")
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
