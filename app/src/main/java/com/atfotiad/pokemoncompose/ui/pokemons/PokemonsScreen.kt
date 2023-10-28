22b8c03d31a081021a410bd92e94cdb94ae90ae3
atfotiad <thfotiad@gmail.com>
2023-10-28 07:58:23 +0300

    Pass viewModel instance and call on click

--- app/src/main/java/com/atfotiad/pokemoncompose/ui/pokemons/PokemonsScreen.kt
+++ app/src/main/java/com/atfotiad/pokemoncompose/ui/pokemons/PokemonsScreen.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.atfotiad.pokemoncompose.model.Pokemon
 import com.atfotiad.pokemoncompose.ui.PokemonViewModel
@@ -12,17 +11,21 @@ import com.atfotiad.pokemoncompose.ui.PokemonViewModel
 @Composable
 fun PokemonScreen(
     modifier: Modifier = Modifier,
-    pokemonViewModel: PokemonViewModel = hiltViewModel()
+    pokemonViewModel: PokemonViewModel,
+    onPokemonClick: (Pokemon) -> Unit = {}
 ) {
     Column(modifier) {
         val list = pokemonViewModel.pokemonList.collectAsLazyPagingItems()
         PokemonList(
             list = list
         ) { pokemon: Pokemon ->
-            Log.i(
-                "Poke",
-                "PokemonItem: Clicked ${pokemon.name}"
-            )
+            run {
+                Log.i(
+                    "Poke",
+                    "PokemonItem: Clicked ${pokemon.name}"
+                )
+                onPokemonClick(pokemon)
+            }
         }
     }
 }
