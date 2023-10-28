fb0a98f6526b1dce8ad1076524db6a6bd0762a32
atfotiad <thfotiad@gmail.com>
2023-10-28 07:54:29 +0300

    Clean up MainActivity

--- app/src/main/java/com/atfotiad/pokemoncompose/MainActivity.kt
+++ app/src/main/java/com/atfotiad/pokemoncompose/MainActivity.kt
@@ -3,15 +3,9 @@ package com.atfotiad.pokemoncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.lifecycleScope
-import com.atfotiad.pokemoncompose.ui.pokemons.PokemonScreen
+import com.atfotiad.pokemoncompose.PokemonComposeApplication.Companion.PokeApp
 import com.atfotiad.pokemoncompose.ui.theme.PokemonComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -19,13 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokemonComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    PokemonScreen()
-                }
+                PokeApp()
             }
         }
     }
