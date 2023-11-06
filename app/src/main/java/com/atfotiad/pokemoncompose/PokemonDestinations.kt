8f00ce2e98236a2fc8b51ce82f711ed7073de9d3
atfotiad <thfotiad@gmail.com>
2023-11-06 15:25:23 +0200

    Update the destinations

--- app/src/main/java/com/atfotiad/pokemoncompose/PokemonDestinations.kt
+++ app/src/main/java/com/atfotiad/pokemoncompose/PokemonDestinations.kt
@@ -1,21 +1,42 @@
 package com.atfotiad.pokemoncompose
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
 interface PokemonDestination {
+    val icon: ImageVector
     val route: String
 }
 
 object PokemonList : PokemonDestination {
+    override val icon: ImageVector = Icons.Filled.List
     override val route: String = "pokemonList"
 }
 
 object SinglePokemonScreen : PokemonDestination {
+    override val icon: ImageVector = Icons.Filled.Person
     override val route: String = "singlePokemon"
     const val pokemonArg = "pokemonName"
     val routeWithArgs = "${route}/{${pokemonArg}}"
-    val arguments = listOf(
-        navArgument(pokemonArg) { type = NavType.StringType }
-    )
+    val arguments = listOf(navArgument(pokemonArg) { type = NavType.StringType })
 }
+
+object RequestFromContentUriScreen : PokemonDestination {
+    override val icon: ImageVector = Icons.Filled.Search
+    override val route: String = "Upload"
+}
+
+object CameraScreen: PokemonDestination {
+    override val icon: ImageVector = Icons.Filled.CameraAlt
+    override val route: String = "Camera"
+
+
+}
+
+val pokemonTabRowScreens = listOf(PokemonList, RequestFromContentUriScreen)
