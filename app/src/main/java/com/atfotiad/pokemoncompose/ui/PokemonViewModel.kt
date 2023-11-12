1a204f9122e58ebcdce526a89fab9546d3466952
atfotiad <thfotiad@gmail.com>
2023-11-13 00:23:01 +0200

    Update PokemonViewModel to use offline functions

--- app/src/main/java/com/atfotiad/pokemoncompose/ui/PokemonViewModel.kt
+++ app/src/main/java/com/atfotiad/pokemoncompose/ui/PokemonViewModel.kt
@@ -1,17 +1,43 @@
 package com.atfotiad.pokemoncompose.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import com.atfotiad.pokemoncompose.model.OfficialArtwork
+import com.atfotiad.pokemoncompose.model.Other
+import com.atfotiad.pokemoncompose.model.Pokemon
+import com.atfotiad.pokemoncompose.model.Sprites
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonViewModel @Inject constructor(
-    repository: PokeRepository
+    private val repository: PokeRepository
 ) : ViewModel() {
 
-    val pokemonList =
-        repository.getPokeResults().cachedIn(viewModelScope)
+    var pokemon: Pokemon? by mutableStateOf(
+        Pokemon(
+            "",
+            Sprites(Other(OfficialArtwork(""))),
+            arrayListOf(),
+            ""
+        )
+    )
+    val pokemonList = repository.getPokeResults().cachedIn(viewModelScope)
 
+    fun insertPokemon(pokemon: Pokemon) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.localDataSource.insertPokemon(pokemon)
+        }
+    }
+
+    fun getAllPokemonFromDatabase(): Flow<List<Pokemon>> {
+        return repository.localDataSource.getAllPokemon()
+    }
 }
