0139c1c71dc8ead015eb8de321bf374d619a9f02
atfotiad <thfotiad@gmail.com>
2023-11-13 00:20:34 +0200

    Update PokemonDetailsScreen

--- app/src/main/java/com/atfotiad/pokemoncompose/ui/pokemons/PokemonDetailsScreen.kt
+++ app/src/main/java/com/atfotiad/pokemoncompose/ui/pokemons/PokemonDetailsScreen.kt
@@ -8,32 +8,74 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CatchingPokemon
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.atfotiad.pokemoncompose.model.Pokemon
+import com.atfotiad.pokemoncompose.ui.PokemonViewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun PokemonDetailsScreen(
     modifier: Modifier = Modifier,
+    viewModel: PokemonViewModel,
     pokemon: Pokemon
-) {
+    ) {
+    val coroutineScope = rememberCoroutineScope()
     LaunchedEffect(pokemon) {
         Log.i("pokemon", "SinglePokemon: ${pokemon.name}")
+        Log.i("viewModel", "PokemonDetailsScreen: $viewModel")
     }
-    SinglePokemon(pokemon, modifier)
+
+    ConstraintLayout(Modifier.verticalScroll(rememberScrollState(), true)) {
+        val (img, fab) = createRefs()
+        SinglePokemon(pokemon, modifier.constrainAs(img) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+        })
+        FloatingActionButton(modifier = Modifier.constrainAs(fab) {
+            top.linkTo(img.bottom, margin = (-48).dp)
+            end.linkTo(img.end, margin = 16.dp)
+        },
+            elevation = FloatingActionButtonDefaults.elevation(),
+            shape = CircleShape,
+            containerColor = Color.Red,
+            onClick = {
+                coroutineScope.launch(Dispatchers.IO) {
+                    viewModel.insertPokemon(pokemon)
+                }
+            }) {
+            Icon(
+                Icons.Filled.CatchingPokemon, contentDescription = "Floating action button"
+            )
+        }
+    }
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)
